b99b8ab71f1914db6c96c015b99e1a241350d2fa
Robur49 <240334205+Robur49@users.noreply.github.com>
2026-09-15 16:56:34 -0700

    Remove bottom lines train_model.py

--- train_model.py
+++ train_model.py
@@ -32,6 +32,3 @@ y_pred = rf_classifier. predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy * 100: .2f}%")
 print(confusion_matrix(y_test, y_pred))
-
-#Minute 23
-
